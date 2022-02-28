052e68f36450bb1d468c804941ddd450dd174c54
Junichi Kato <j5ik2o@gmail.com>
2022-02-28 09:59:47 +0900

    Add doc to cron_schedule.rs.

--- src/cron_schedule.rs
+++ src/cron_schedule.rs
@@ -1,9 +1,13 @@
+use std::marker::PhantomData;
+
+use anyhow::Result;
 use chrono::{DateTime, TimeZone};
 use intervals_rs::LimitValue;
 
-use crate::{CronInterval, Expr, CronSpecification, CronParser, CronIntervalIterator};
-use std::marker::PhantomData;
+use crate::{CronInterval, CronIntervalIterator, CronParser, CronSpecification, Expr};
 
+/// Facade that returns a CronInterval or CronIntervalIterator from a CROND string.<br/>
+/// CROND文字列からCronIntervalやCronIntervalIteratorを返すFacade。
 pub struct CronSchedule<Tz>
 where
   Tz: TimeZone,
@@ -13,19 +17,39 @@ where
 }
 
 impl<Tz: TimeZone> CronSchedule<Tz> {
-  pub fn new(source: &str) -> Self {
-    let expr = CronParser::parse(source).unwrap();
-    Self {
-      expr,
+  /// The Factory method.
+  /// ファクトリメソッド.
+  ///
+  /// # Arguments(引数)
+  ///
+  /// * crond_string - CROND形式文字列
+  ///
+  /// # Retun values(戻り値)
+  ///
+  /// * Err:
+  ///   - If CrondParser::parse fails
+  ///   - CrondParser::parseに失敗した場合
+  /// * Ok
+  ///   - If CrondParser::parse succeeds
+  ///   - CrondParser::parseに成功した場合
+  pub fn new(crond_string: &str) -> Result<Self> {
+    Ok(Self {
+      expr: CronParser::parse(crond_string)?,
       phantom: PhantomData,
-    }
+    })
   }
 
+  /// Returns a CronInterval with date and time candidates after the start date and time.<br/>
+  /// 開始日時以降の日時候補を持つCronIntervalを返す。
   pub fn cron_interval(&self, start: DateTime<Tz>) -> CronInterval<Tz, CronSpecification> {
     let spec = CronSpecification::new(self.expr.clone());
-    CronInterval::new(LimitValue::Limit(start), LimitValue::Limitless, spec)
+    let start = LimitValue::Limit(start);
+    let end = LimitValue::Limitless;
+    CronInterval::new(start, end, spec)
   }
 
+  /// Returns a CronIntervalIterator with the date and time candidates after the start date and time.<br/>
+  /// 開始日時以降の日時候補を持つCronIntervalIteratorを返す。
   pub fn upcoming(&self, start: DateTime<Tz>) -> CronIntervalIterator<Tz, CronSpecification> {
     self.cron_interval(start.clone()).iter(start.timezone())
   }
@@ -33,18 +57,23 @@ impl<Tz: TimeZone> CronSchedule<Tz> {
 
 #[cfg(test)]
 mod tests {
+  use super::*;
   use chrono::{TimeZone, Utc};
-  use intervals_rs::LimitValue;
-
-  use crate::{CronInterval, CronParser, CronSchedule, CronSpecification};
 
   #[test]
   fn test_iterator() {
-    let dt = Utc.ymd(2021, 1, 1).and_hms(1, 1, 0);
+    let dt: chrono::DateTime<Utc> = Utc.ymd(2021, 1, 1).and_hms(1, 1, 0);
 
-    let cs = CronSchedule::new("0-59/30 0-23/2 * * *");
-    let itr = cs.upcoming(dt);
+    let cs: CronSchedule<Utc> = CronSchedule::new("0-59/30 0-23/2 * * *").unwrap();
+    let itr: crate::CronIntervalIterator<Utc, CronSpecification> = cs.upcoming(dt);
 
     itr.take(5).for_each(|e| println!("{:?}", e));
+    /*
+2021-01-01T02:00:00Z
+2021-01-01T02:30:00Z
+2021-01-01T04:00:00Z
+2021-01-01T04:30:00Z
+2021-01-01T06:00:00Z
+    */
   }
 }
